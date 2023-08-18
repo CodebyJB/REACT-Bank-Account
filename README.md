2d531bd966571f34211b45f3f1dd74e821435584
Jennifer <106908910+JenniferBraunbarth@users.noreply.github.com>
2023-08-18 13:16:45 +0200

    first commit

--- README.md
+++ README.md
@@ -1,70 +1,50 @@
-# Getting Started with Create React App
+````markdown
+# Bank Account Management with useReducer and React
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project is a simple bank account management application built using React and the `useReducer` hook. It allows users to open an account, deposit, withdraw, request loans, pay loans, and close accounts.
 
-## Available Scripts
+## Features
 
-In the project directory, you can run:
+- Open an account with an initial bonus balance.
+- Deposit funds into the account.
+- Withdraw funds from the account.
+- Request loans, adding the loan amount to the account balance.
+- Pay back loans, subtracting the loan amount from the account balance.
+- Close the account if the balance is zero and there are no outstanding loans.
 
-### `npm start`
+## Getting Started
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+1. Clone this repository to your local machine.
+2. Install the required dependencies using the following command:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+```sh
+npm install
+```
+````
 
-### `npm test`
+3. Start the development server:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+```sh
+npm start
+```
 
-### `npm run build`
+The application will be accessible at `http://localhost:3000`.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Usage
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+1. Click the "Open Account" button to start.
+2. Use the input fields to provide necessary information for each action:
+   - Deposit: Enter the amount you want to deposit and click the "Deposit" button.
+   - Withdraw: Enter the amount you want to withdraw and click the "Withdraw" button.
+   - Request a Loan: Enter the amount you want to request as a loan and click the "Request a loan" button.
+   - Pay Loan: Enter the amount you want to pay back and click the "Pay loan" button.
+3. The account's balance and loan information will be displayed on the page.
+4. You can perform multiple actions in succession.
+5. Click the "Close Account" button to close the account if the balance is zero and there are no outstanding loans.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Technologies Used
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- React
+- `useReducer` Hook
+- HTML
+- CSS
--- src/App.js
+++ src/App.js
@@ -1,25 +1,180 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from "react";
 
-function App() {
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: false,
+  msg: "Open your account!",
+};
+
+const START_BONUS = 50;
+
+function reducer(state, action) {
+  if (!state.isActive && action.type !== "openAccount") return state;
+
+  switch (action.type) {
+    case "openAccount":
+      return {
+        ...state,
+        isActive: true,
+        balance: START_BONUS,
+        msg: `Welcome, you receive ${START_BONUS} € as a bonus to start`,
+      };
+    case "deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        msg: `You deposit ${action.payload} €`,
+      };
+    case "withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+        msg: `You withdraw ${action.payload} €`,
+      };
+    case "requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload,
+        balance: state.balance + action.payload,
+        msg: `You request ${action.payload} € loan`,
+      };
+    case "payLoan":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+        loan: state.loan - action.payload,
+        msg: `You pay ${action.payload} € loan`,
+      };
+    case "closeAccount":
+      if (state.balance !== 0 || state.loan > 0) return state;
+      return initialState;
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { balance, loan, isActive, msg } = state;
+  const [depositAmount, setDepositAmount] = useState("");
+  const [withdrawAmount, setWithdrawAmount] = useState("");
+  const [requestLoanAmount, setRequestLoanAmount] = useState("");
+  const [payLoanAmount, setPayLoanAmount] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>useReducer Bank Account</h1>
+      <p className="msg">{msg}</p>
+      <p>
+        Balance:
+        <span style={{ color: balance >= 0 ? "green" : "red" }}>
+          {" "}
+          {balance} €
+        </span>
+      </p>
+      <p>
+        Loan:
+        <span style={{ color: balance >= 0 ? "green" : "red" }}> {loan} €</span>
+      </p>
+
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "openAccount" });
+          }}
+          disabled={isActive}
         >
-          Learn React
-        </a>
-      </header>
+          Open account
+        </button>
+      </p>
+      <p>
+        <input
+          type="number"
+          disabled={!isActive}
+          value={depositAmount}
+          onChange={(e) => {
+            setDepositAmount(Number(e.target.value));
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch({ type: "deposit", payload: depositAmount });
+            setDepositAmount("");
+          }}
+          disabled={!isActive}
+        >
+          Deposit
+        </button>
+      </p>
+      <p>
+        <input
+          type="number"
+          disabled={!isActive}
+          value={withdrawAmount}
+          onChange={(e) => {
+            setWithdrawAmount(Number(e.target.value));
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch({ type: "withdraw", payload: withdrawAmount });
+            setWithdrawAmount("");
+          }}
+          disabled={!isActive}
+        >
+          Withdraw
+        </button>
+      </p>
+      <p>
+        <input
+          type="number"
+          disabled={!isActive}
+          value={requestLoanAmount}
+          onChange={(e) => {
+            setRequestLoanAmount(Number(e.target.value));
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch({ type: "requestLoan", payload: requestLoanAmount });
+            setRequestLoanAmount("");
+          }}
+          disabled={!isActive}
+        >
+          Request a loan
+        </button>
+      </p>
+      <p>
+        <input
+          type="number"
+          disabled={!isActive}
+          value={payLoanAmount}
+          onChange={(e) => {
+            setPayLoanAmount(Number(e.target.value));
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch({ type: "payLoan", payload: payLoanAmount });
+            setPayLoanAmount("");
+          }}
+          disabled={!isActive}
+        >
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button
+          onClick={() => {
+            dispatch({ type: "closeAccount" });
+          }}
+          disabled={!isActive}
+        >
+          Close account
+        </button>
+      </p>
     </div>
   );
 }
-
-export default App;
